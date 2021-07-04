120df0e8df62ef8264f7251e065ce53cf939817a
RAHUL KUMAWAT <suryarising17@gmail.com>
2021-07-04 17:53:40 +0530

    thirdCommint

--- pal/pal/settings.py
+++ pal/pal/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -112,5 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 MEDIA_URL='/media/' 
 MEDIA_ROOT=BASE_DIR/'media'
